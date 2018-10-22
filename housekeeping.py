088ccc89eb98f013441b75e4a3aa5fc39669c92a
Massimo M <massimo.minuti@pi.infn.it>
2018-10-22 20:23:00 +0200

    sistemata indentazione

--- housekeeping.py
+++ housekeeping.py
@@ -7,24 +7,22 @@ def get_time():
 	return time
 
 def list_entries():
-    
-    switches_file = open('hk_switches.txt', 'r')
-    entry = switches_file.read()
-    switch = json.loads(entry)
-    switches_file.close()
-    
-    return switch
+	switches_file = open('hk_switches.txt', 'r')
+	entry = switches_file.read()
+	switch = json.loads(entry)
+	switches_file.close()
+	return switch
 
 def start_gui():
-        print ('The GUI should be loaded here')
+	print ('The GUI should be loaded here')
         
 if (__name__ == "__main__"):
 
 	parser = argparse.ArgumentParser( description='Perform Housekeeping tasks');
 	
 	parser.add_argument('--time', action='store_true',	help = 'get the actual time')
-        parser.add_argument('--gui', action='store_true',	help = 'start GUI')
-        parser.add_argument('--list', action='store_true',	help = 'list switches table')
+	parser.add_argument('--gui', action='store_true',	help = 'start GUI')
+	parser.add_argument('--list', action='store_true',	help = 'list switches table')
 	
 	args = parser.parse_args()
 
